3ee2eae1738e07bd1b70a888ca4185491f478f97
ichikw <54463952+ichikw@users.noreply.github.com>
2026-07-16 19:58:33 +0900

    OwnedGoodControllerのマッピング修正

--- backend/src/main/java/com/favexchange/backend/controller/OwnedGoodController.java
+++ backend/src/main/java/com/favexchange/backend/controller/OwnedGoodController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/goods")
 @CrossOrigin(
         origins = {
                 "http://localhost:5173",
